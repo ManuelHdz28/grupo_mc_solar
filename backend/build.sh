10334f37d93eae8d324418d7aa53c0745ef248bb
manuel <manuel.hernandez28@outlook.com>
2025-07-17 23:08:37 -0600

    Modificar script build.sh para crear superusuario solo si no existe

--- backend/build.sh
+++ backend/build.sh
@@ -7,8 +7,15 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-python manage.py createsuperuser \
-  --noinput \
-  --username $DJANGO_SUPERUSER_USERNAME \
-  --email $DJANGO_SUPERUSER_EMAIL
+# Crear superusuario solo si no existe
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+username = '$DJANGO_SUPERUSER_USERNAME'; \
+email = '$DJANGO_SUPERUSER_EMAIL'; \
+password = '$DJANGO_SUPERUSER_PASSWORD'; \
+\
+User.objects.filter(username=username).exists() or \
+User.objects.create_superuser(username=username, email=email, password=password)" \
+| python manage.py shell
+
 
